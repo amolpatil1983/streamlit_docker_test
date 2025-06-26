7d3c0a43651cdc62db000a9a81bb4e3ca0aaa8b6
Patil Amol Baliram <amol.patil.phd@gmail.com>
2025-06-27 00:09:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,21 +7,20 @@ st.set_page_config(page_title="CrippenLogP 2D Map Tool", layout="centered")
 
 st.title("Explore Molecular Lipophilicity with Our 2D CrippenLogP Map Tool")
 
-st.header("What It Does")
+st.write("*What It Does*")
 st.write("Generates a static 2D map visualizing atomic contributions to a molecule's CrippenLogP value, highlighting how each atom affects lipophilicity.")
 
-st.header("Scientific Basis")
+st.write("*Scientific Basis*")
 st.write("Based on Crippen's LogP method [Wildman & Crippen, 1999](https://doi.org/10.1021/ci990307l), this tool calculates hydrophobicity, a critical factor in drug solubility, membrane permeability, and bioavailability.")
 
-st.header("Why It Matters")
-st.write("Mapping LogP contributions helps optimize molecular structures for improved ADME (absorption, distribution, metabolism, excretion) properties in drug development [Lipinski, 2004](https://doi.org/10.1016/S1359-6446(03)02671-2).")
+st.write("*Why It Matters*")
+st.write("Mapping LogP contributions helps optimize molecular structures for improved ADME (absorption, distribution, metabolism, excretion) properties in drug development [Lipinski, 2004](https://www.sciencedirect.com/science/article/abs/pii/S0169409X00001290?via%3Dihub).")
 
-st.header("How to Use")
-st.write("Input a valid SMILES string of your molecule in the input field below and press 'Enter.' Need a SMILES string? Use the [PubChem Sketcher](https://pubchem.ncbi.nlm.nih.gov/#input=draw) to draw your molecule and generate its SMILES.")
+st.write("*How to Use*")
+st.write("Input a valid SMILES string of your molecule in the input field below and press 'Enter.' Need a SMILES string? Use the [PubChem Sketcher](https://pubchem.ncbi.nlm.nih.gov/#input=draw&draw=true) to draw your molecule and generate its SMILES.")
 
-st.header("Analyzing Results")
+st.write("*Analyzing Results*")
 st.write("The static map color-codes atoms by LogP contribution (positive: hydrophobic, negative: hydrophilic). Review the legend for value ranges. Identify key structural features influencing lipophilicity to guide molecular design.")
-
 st.markdown("*Start mapping your molecule’s potential today!*", unsafe_allow_html=True)
 # User input for SMILES string
 smiles = st.text_input("Enter SMILES string", "O=C(O)C[C@H](O)C[C@H](O)CCn2c(c(c(c2c1ccc(F)cc1)c3ccccc3)C(=O)Nc4ccccc4)C(C)C")
