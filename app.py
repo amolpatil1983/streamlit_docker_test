92115b2fa4dcd09f5237d7b086f64887f8ea7dc5
Patil Amol Baliram <amol.patil.phd@gmail.com>
2025-06-27 00:22:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 import streamlit as st
-from utils.molecule_utils import smiles_to_mol_image, calculate_mol_weight, get_rdk_mol_from_sml, get_atomic_contributions_to_logp 
+from utils.molecule_utils import smiles_to_mol_image, calculate_mol_weight, get_rdk_mol_from_sml, get_atomic_contributions_to_logp
 
-import streamlit as st
+st.set_page_config(layout="wide")
+#st.write(f"✅ Reloaded at: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+st.info("This is a beta version under active development.")
 
 st.title(":rainbow[Cheminformatics Assistant]")
 
-st.write("*What It Does*")
-st.write("Generates a static 2D map visualizing atomic contributions to a molecule's CrippenLogP value, highlighting how each atom affects lipophilicity.")
-st.write("*Why It Matters*")
-st.write("Mapping LogP contributions helps optimize molecular structures for improved ADME (absorption, distribution, metabolism, excretion) properties in drug development [Lipinski, 2004](https://www.sciencedirect.com/science/article/abs/pii/S0169409X00001290?via%3Dihub).")
-st.write("*Scientific Basis*")
-st.write("Based on Crippen's LogP method [Wildman & Crippen, 1999](https://doi.org/10.1021/ci990307l), this tool calculates hydrophobicity, a critical factor in drug solubility, membrane permeability, and bioavailability.")
-st.write("*How to Use*")
-st.write("Input a valid SMILES string of your molecule in the input field below and press 'Enter.' Need a SMILES string? Use the [PubChem Sketcher](https://pubchem.ncbi.nlm.nih.gov/#input=draw&draw=true) to draw your molecule and generate its SMILES.")
-st.write("*Analyzing Results*")
-st.write("The static map color-codes atoms by LogP contribution (Green: hydrophobic, Red: hydrophilic). Identify key structural features influencing lipophilicity to guide molecular design.")
+st.success("*What It Does*")
+st.info("Generates a static 2D map visualizing atomic contributions to a molecule's CrippenLogP value, highlighting how each atom affects lipophilicity.")
+st.success("*Why It Matters*")
+st.info("Mapping LogP contributions helps optimize molecular structures for improved ADME (absorption, distribution, metabolism, excretion) properties in drug development [Lipinski, 2004](https://www.sciencedirect.com/science/article/abs/pii/S0169409X00001290?via%3Dihub).")
+st.success("*Scientific Basis*")
+st.info("Based on Crippen's LogP method [Wildman & Crippen, 1999](https://doi.org/10.1021/ci990307l), this tool calculates hydrophobicity, a critical factor in drug solubility, membrane permeability, and bioavailability.")
+st.success("*How to Use*")
+st.info("Input a valid SMILES string of your molecule in the input field below and press 'Enter.' Need a SMILES string? Use the [PubChem Sketcher](https://pubchem.ncbi.nlm.nih.gov/#input=draw&draw=true) to draw your molecule and generate its SMILES.")
+st.success("*Analyzing Results*")
+st.info("The static map color-codes atoms by LogP contribution (Green: hydrophobic, Red: hydrophilic). Identify key structural features influencing lipophilicity to guide molecular design.")
 st.markdown("*Start mapping your molecule’s potential today!*", unsafe_allow_html=True)
 
 # User input for SMILES string
