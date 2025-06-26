8097edc779ae74f99cf97e404cada7408f5de6a2
Patil Amol Baliram <amol.patil.phd@gmail.com>
2025-06-27 00:48:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,13 @@ st.set_page_config(layout="wide")
 st.title(":rainbow[Cheminformatics Assistant]")
 
 st.info("""
-**What it does:** Generates a static 2D map visualizing atomic contributions to a molecule's Crippen LogP value, highlighting how each atom affects lipophilicity.
-
-**Why it matters:** Mapping LogP contributions helps optimize molecular structures for improved ADME (absorption, distribution, metabolism, excretion) properties in drug development. [Lipinski, 2004](https://www.sciencedirect.com/science/article/abs/pii/S0169409X00001290?via%3Dihub)
-
+**What it does?:** Generates a static 2D map visualizing atomic contributions to a molecule's Crippen LogP value, highlighting how each atom affects lipophilicity.
+**Why it matters?:** Mapping LogP contributions helps optimize molecular structures for improved ADME (absorption, distribution, metabolism, excretion) properties in drug development. [Lipinski, 2004](https://www.sciencedirect.com/science/article/abs/pii/S0169409X00001290?via%3Dihub)
 **Scientific basis:** Based on Crippen's LogP method [Wildman & Crippen, 1999](https://doi.org/10.1021/ci990307l), this tool calculates hydrophobicity — a critical factor in drug solubility, membrane permeability, and bioavailability.
-
-**How to use:** Input a valid SMILES string of your molecule in the input field below and press 'Enter'. Need a SMILES string? Use the [PubChem Sketcher](https://pubchem.ncbi.nlm.nih.gov/#input=draw&draw=true) to draw your molecule and generate its SMILES.
-
+**How to use?:** Input a valid SMILES string of your molecule in the input field below and press 'Enter'. Need a SMILES string? Use the [PubChem Sketcher](https://pubchem.ncbi.nlm.nih.gov/#input=draw&draw=true) to draw your molecule and generate its SMILES.
 **Analyzing results:** The static map color-codes atoms by LogP contribution.  
 Green = hydrophobic, Red = hydrophilic.  
 Identify key structural features influencing lipophilicity to guide molecular design.
-
 **Start mapping your molecule’s potential today!**
 """)
 
