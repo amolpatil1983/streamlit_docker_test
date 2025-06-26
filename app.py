c67816d27abfc6f10eebd7d90149a330f00f7b2b
Patil Amol Baliram <amol.patil.phd@gmail.com>
2025-06-27 01:09:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title(":rainbow[Cheminformatics Assistant]")
 col1, col2 = st.columns([1, 2])
 
 with col1:
-    st.image(get_atomic_contributions_to_logp(get_rdk_mol_from_sml("O=C(O)C[C@H](O)C[C@H](O)CCn2c(c(c(c2c1ccc(F)cc1)c3ccccc3)C(=O)Nc4ccccc4)C(C)C")), caption="Example Atomic Contributions to LogP, Atorvastatin", use_column_width=True)
+    st.image(get_atomic_contributions_to_logp(get_rdk_mol_from_sml("O=C(O)C[C@H](O)C[C@H](O)CCn2c(c(c(c2c1ccc(F)cc1)c3ccccc3)C(=O)Nc4ccccc4)C(C)C")), caption="Example Atomic Contributions to LogP, Atorvastatin", use_container_width=True)
 
 with col2:
     st.info("""
