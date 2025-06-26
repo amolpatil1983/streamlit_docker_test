06211f5a6c462bea16d35e0a637824085955bdc3
Patil Amol Baliram <amol.patil.phd@gmail.com>
2025-06-26 19:58:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from utils.molecule_utils import smiles_to_mol_image, calculate_mol_weight, get_rdk_mol_from_sml, 
+from utils.molecule_utils import smiles_to_mol_image, calculate_mol_weight, get_rdk_mol_from_sml, get_atomic_contributions_to_logp 
 
 st.title("RDKit + Sklearn Streamlit App")
 
@@ -17,11 +17,7 @@ if smiles:
     if mol_weight:
         st.write(f"Molecular Weight: {mol_weight:.2f} g/mol")
     
-    # Predict solubility using sklearn model
-    solubility = predict_solubility(smiles)
-    if solubility:
-        st.write(f"Predicted Solubility (logS): {solubility:.2f}")
-    
     rdkmolobj = smiles_to_rdk_mol(smiles)
     if rdkmolobj:
+        get_atomic_contributions_to_logp(rdkmolobj)
         
